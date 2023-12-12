0d10b2310284d627b978d1a50406c7739e93b025
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-12-12 15:17:47 +0100

    Ajout de l'examen

---
+++ CeUaa14Partie2_dec23_VictorPholien/CeUaa14Partie2_dec23_VictorPholien/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace CeUaa14Partie2_dec23_VictorPholien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int poche = 30;
+            int chargeur = 0;
+            strin reponse;
+            bool continuer = true;
+            string balles = "Vous avez un total de 30 balles dans votre poche et 0 balles dans le chargeur";
+            Console.WriteLine("Bienvenue dans ce jeu de tir . . . Vous démarrez avec 30 balles");
+            Console.WriteLine("===============================================================");
+            while (continuer == true)
+            {
+                Console.WriteLine(balles);
+                Console.WriteLine("Espace pour tirer,");
+                Console.WriteLine("r pour recharger,");
+                Console.WriteLine("+ pour reprendre des munitions,");
+                Console.WriteLine("q pour quitter");
+                Console.ReadLine();
+                if(reponse = "r") 
+                {
+                    Console.Write(paintballGun.reload);
+                }
+                if(reponse = "Espace")
+                {
+                    Console.Write(paintballGun.tirer);
+                }
+                if(reponse = "+")
+                {
+                    Console.Write(paintballGun.reprendreMunition);
+                }
+                if (reponse = "q")
+                {
+                    Console.Write(paintballGun.quitter);
+                }
+                
+            }
+        }
+    }
+}
---
+++ CeUaa14Partie2_dec23_VictorPholien/CeUaa14Partie2_dec23_VictorPholien/paintballGun.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeUaa14Partie2_dec23_VictorPholien
+{
+    internal class paintballGun
+    {
+        /// <summary>
+        /// Permet de recharger l'arme
+        /// </summary>
+        /// <param name="chargeur">Le chargeur</param>
+        /// <param name="balles">le nombre de balle</param>
+        public void reload (int chargeur, int balles)
+        {
+                chargeur = chargeur + 16;
+                if (chargeur > 30)
+                {
+                    Console.WriteLine("Vous ne pouvez pas avoir plus de 30 balles dans votre chargeur");
+                }
+                else
+                {
+                    Console.WriteLine(balles);
+                }  
+        }
+
+        /// <summary>
+        /// permet de tirer avec espace
+        /// </summary>
+        /// <param name="chargeur">Le chargeur de l'arme</param>
+        /// <param name="balles">Le nombre de balles</param>
+        public void tirer(int chargeur, int balles)
+        {
+            if (chargeur <= 0)
+            {
+                Console.WriteLine("Vous n'avez pas de balles dans votre chargeur");
+            }
+            else
+            {
+                Console.WriteLine("=> Tir effectué");
+                Console.WriteLine(balles);
+            }
+        }
+
+        /// <summary>
+        /// Reprendre des munitions dans la poche
+        /// </summary>
+        /// <param name="poche">poche contenant les balles</param>
+        /// <param name="balles">nombre de balle</param>
+        public void reprendreMunition(int poche, int balles)
+        {
+            poche = poche + 30;
+            Console.WriteLine(balles);
+        }
+
+        /// <summary>
+        /// Permet de quitter le programme
+        /// </summary>
+        /// <param name="continuer">boucle pour continuer</param>
+        public void quitter(bool continuer)
+        {
+            continuer = false;
+        }
+    }
+}
+
+
+
